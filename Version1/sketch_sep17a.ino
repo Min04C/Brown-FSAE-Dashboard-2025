cd07d2ca68e55d0db92665d0063390767139d4ec
Min04C <123710130+Min04C@users.noreply.github.com>
2024-09-17 14:52:18 +0900

    Add files via upload

---
+++ Version1/sketch_sep17a.ino
@@ -0,0 +1,10 @@
+// Dashboard 2025 Code
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("hi");
+}
